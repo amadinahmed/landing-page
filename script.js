681278b1c09106a22259b6a2101971560d9a8511
Amadin Ahmed <48179347+amadinahmed@users.noreply.github.com>
2024-06-10 18:03:46 +0000

    Wireframe Structure

--- script.js
+++ script.js
@@ -1,26 +1,29 @@
-const form = document.querySelector("#form");
-const submitButton = document.querySelector("#submit");
-const formTitle = document.getElementById("formTitle");
-const card = document.getElementById("card");
-const spinner = document.getElementById("spinner");
-const scriptURL = "https://script.google.com/macros/s/AKfycbwd5g0oRx2kkgjf5hdoAbQsjgaT77bm54tUM4gEwmtFg3ehGpV7GJDVlGuL4iQJQcesGg/exec";
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("joinForm");
+  const submitButton = form.querySelector("button[type='submit']");
+  const spinner = document.getElementById("spinner");
+  const checkmark = document.querySelector(".checkmark");
+  const hero = document.querySelector(".hero");
+  const scriptURL = "https://script.google.com/macros/s/AKfycbwd5g0oRx2kkgjf5hdoAbQsjgaT77bm54tUM4gEwmtFg3ehGpV7GJDVlGuL4iQJQcesGg/exec";
 
-form.addEventListener('submit', e => {
-  submitButton.disabled = true;
-  card.classList.add('loading');
-  e.preventDefault();
-  let requestBody = new FormData(form);
-  fetch(scriptURL, { method: 'POST', body: requestBody })
-    .then(response => {
-      card.classList.remove('loading');
-      card.classList.add('success');
-      formTitle.textContent = "Successfully sent";
-      form.style.display = "none";
-      submitButton.disabled = false;
-    })
-    .catch(error => {
-      console.error('Error:', error.message);
-      card.classList.remove('loading');
-      submitButton.disabled = false;
-    });
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    submitButton.disabled = true;
+    hero.classList.add("loading");
+
+    let requestBody = new FormData(form);
+    fetch(scriptURL, { method: "POST", body: requestBody })
+      .then(response => {
+        hero.classList.remove("loading");
+        hero.classList.add("success");
+        submitButton.disabled = false;
+        form.reset();
+        checkmark.style.display = "block";
+      })
+      .catch(error => {
+        console.error("Error:", error.message);
+        hero.classList.remove("loading");
+        submitButton.disabled = false;
+      });
+  });
 });
